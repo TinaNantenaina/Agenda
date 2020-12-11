128634b64bb8be22aa02fdc635259cbeb6591b5e
Julie LEROY <Julie LEROY@192.168.1.2>
2020-12-11 21:15:16 +0100

    service enseignant

--- src/main/java/agenda/Agenda.java
+++ src/main/java/agenda/Agenda.java
@@ -1,24 +1,23 @@
 package agenda;
 
 import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.*;
 
 /**
  * Description : An agenda that stores events
  */
 public class Agenda {
+    
+     public HashSet<Event> Events = new HashSet<Event>();
     /**
      * Adds an event to this agenda
      *
      * @param e the event to add
      */
     
-    List<Event> agenda = new LinkedList<>();
-    
     public void addEvent(Event e) {
-        // méthode implémentée
-        agenda.add(e);
-    }
+        this.Events.add(e);
 
     /**
      * Computes the events that occur on a given day
